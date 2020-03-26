79652c11d5bc2fb5053d50e90889bb6c3f9b7701
idanreicher <idanreicher@gmail.com>
2020-03-26 18:45:59 +0200

    magic

--- site/magicMethods.php
+++ site/magicMethods.php
@@ -99,3 +99,80 @@ $user::hello('fdS' , 25);
 $user::bonus(456.654);
 $user::salary(6546.36);
  */
+/**
+ * __set()
+ */
+
+class UserSetGet
+{
+    private $data = [
+        'name' => 'kjhkdf',
+        'age' => 'kjhkdf',
+        'salary' => 'kjhkdf'
+    ];
+
+    private $name;
+    protected $age;
+    public $salary;
+
+    public function __set($name , $value)
+    {
+        $this-> data[$name] = $value;
+    }
+
+    public function __isset($name)
+    {
+        $isset = array_key_exists($name , $this->data) ? true : false;
+    }
+
+    public function __unset($name)
+    {
+        unset($this->data[$name]);
+    }
+
+    public function __get($name)
+    {
+        if (array_key_exists($name , $this->data)) {
+            echo '__get => ' . $name . ': ' . $this->data[$name] . PHP_EOL ;
+        }else{
+            trigger_error('Undefined Arg:' . $name , E_USER_NOTICE);
+        }
+    }
+}
+
+/**
+$userSet = new UserSetGet();
+$userSet->name = 'john';
+$userSet->age = 34;
+$userSet->salary = 4200.00;
+$userSet->message = 'hello';
+
+var_dump($userSet);
+
+
+ $userGet = new UserSetGet();
+var_dump(isset($userGet->salary));
+$userGet->salary = 222;
+ echo $userGet->name . PHP_EOL;
+ echo $userGet->age . PHP_EOL;
+ echo $userGet->salary . PHP_EOL;
+ echo $userGet->message . PHP_EOL;
+ var_dump(isset($userGet->salary));
+ echo "<hr>";
+
+ $userUnset = new UserSetGet();
+ 
+ echo 'BEFORE : ' . var_dump( $userUnset);echo "<hr>";
+ unset($userUnset->age);
+ unset($userUnset->name);
+ unset($userUnset->salary);
+ echo "<hr>";
+ echo 'AFTER : ' . var_dump( $userUnset);
+  */
+
+  class UserSleep
+  {
+      public $nam = 'dfas';
+      private $age = 14;
+      protected $salary = 42005283.36;
+  }
